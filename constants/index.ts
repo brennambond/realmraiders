dc84638e4ff150441fda02b215993c20f047e70b
Brennam Bond <brennambond@gmail.com>
2024-04-26 15:47:47 -0500

    Add Constants folder

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const headerLinks = [
+  {
+    label: "HOME",
+    route: "/",
+  },
+  {
+    label: "FIND A RAID",
+    route: "/raids",
+  },
+  { label: "CREATE A RAID", route: "/raids/create" },
+  {
+    label: "MY PROFILE",
+    route: "/profile",
+  },
+];
