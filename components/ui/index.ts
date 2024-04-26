5c9dc8c74072164d291c53a6dc78bd226f0ea966
Brennam Bond <brennambond@gmail.com>
2024-04-26 14:38:23 -0500

    Add Chadcn Ui Components

---
+++ components/ui/index.ts
@@ -0,0 +1,21 @@
+import { AlertDialog } from "./alert-dialog";
+import { Button } from "./button";
+import { Form } from "./form";
+import { Input } from "./input";
+import { Label } from "./label";
+import { Select } from "./select";
+import { Separator } from "./separator";
+import { Sheet } from "./sheet";
+import { Textarea } from "./textarea";
+
+export {
+  AlertDialog,
+  Button,
+  Form,
+  Input,
+  Label,
+  Select,
+  Separator,
+  Sheet,
+  Textarea,
+};
