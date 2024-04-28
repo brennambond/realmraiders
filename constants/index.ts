d682d7ecfc3fa781546d9d93fdb762b6e22ef6bc
Brennam Bond <brennambond@gmail.com>
2024-04-28 18:02:30 -0500

    Update to constants file for default raid values

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,6 @@
+import { IRaid } from "@/lib/database/models/raid.model";
+import { Raid } from "@/types";
+
 export const headerLinks = [
   {
     label: "HOME",
@@ -13,3 +16,62 @@ export const headerLinks = [
     route: "/profile",
   },
 ];
+
+export const raidDefaultValues = {
+  title: "",
+  description: "",
+  environment: "",
+  imageUrl: "",
+  startDateTime: new Date(),
+  endDateTime: new Date(),
+  categoryId: "",
+  price: "",
+  isFeatured: false,
+  url: "",
+};
+
+type raidColorProps = {
+  raid: Raid;
+};
+
+export const raidColors = ({ raid }: raidColorProps) => {
+  const color =
+    raid?.category.name === "S"
+      ? "orange"
+      : raid?.category.name === "A"
+      ? "red"
+      : raid?.category.name === "B"
+      ? "purple"
+      : raid?.category.name === "C"
+      ? "blue"
+      : raid?.category.name === "D"
+      ? "green"
+      : raid?.category.name === "E"
+      ? "brown"
+      : "";
+
+  return color;
+};
+
+type iRaidColorProps = {
+  iRaid: IRaid;
+};
+
+export const iRaidColors = ({ iRaid }: iRaidColorProps) => {
+  const color =
+    iRaid?.category.name === "S"
+      ? "orange"
+      : iRaid?.category.name === "A"
+      ? "red"
+      : iRaid?.category.name === "B"
+      ? "purple"
+      : iRaid?.category.name === "C"
+      ? "blue"
+      : iRaid?.category.name === "D"
+      ? "green"
+      : iRaid?.category.name === "E"
+      ? "brown"
+      : "";
+
+  return color;
+};
